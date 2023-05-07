62ae1f5de6e1c6193ed506310fe200a1c48c2a78
Nikita-Fast <N1kita.fast@yandex.ru>
2023-05-07 15:26:08 +0300

    реализация и тесты статического и динамического алгоритма дейкстры

--- project/task_9_draft.py
+++ project/task_9_draft.py
@@ -1,5 +1,7 @@
+import itertools
 from typing import List
 import networkx as nx
+from boltons.queueutils import HeapPriorityQueue
 
 
 def dijkstra(graph: nx.DiGraph, start_vertex) -> List:
@@ -23,4 +25,133 @@ def dijkstra(graph: nx.DiGraph, start_vertex) -> List:
                 if d[u] + 1 < d[v]:
                     d[v] = d[u] + 1
         visited[u] = True
+
     return d
+
+
+class DynamicSSSP:
+    def __init__(self, graph: nx.DiGraph, start_vertex: int):
+        self.graph: nx.DiGraph = graph
+        self.start_vertex = start_vertex
+        self.modified_vertices = set()
+        self.d = dijkstra(graph, start_vertex)
+
+    def update(self):
+        heap = HeapPriorityQueue(priority_key=lambda x: x)
+        rhs = {}
+        for u in self.modified_vertices:
+            rhs[u] = self.compute_rhs(u)
+            if rhs[u] != self.d[u]:
+                key = min(rhs[u], self.d[u])
+                heap.add(u, key)
+
+        while heap:
+            u = heap.pop()
+            if rhs[u] < self.d[u]:
+                self.d[u] = rhs[u]
+                for v in self.graph.successors(u):
+                    rhs[v] = self.compute_rhs(v)
+                    if rhs[v] != self.d[v]:
+                        key = min(rhs[v], self.d[v])
+                        heap.add(v, key)
+                    else:
+                        if v in heap._entry_map:
+                            heap.remove(v)
+            else:
+                self.d[u] = float("inf")
+                for v in itertools.chain(self.graph.successors(u), [u]):
+                    rhs[v] = self.compute_rhs(v)
+                    if rhs[v] != self.d[v]:
+                        key = min(rhs[v], self.d[v])
+                        heap.add(v, key)
+                    else:
+                        if v in heap._entry_map:
+                            heap.remove(v)
+
+    def remove_edge(self, u, v):
+        self.graph.remove_edge(u, v)
+        self.modified_vertices.add(v)
+
+    def add_edge(self, u, v):
+        self.graph.add_edge(u, v)
+        self.modified_vertices.add(v)
+
+    def compute_rhs(self, v):
+        if v == self.start_vertex:
+            return 0
+        else:
+            return (
+                min(
+                    (self.d[u] for u in self.graph.predecessors(v)),
+                    default=float("inf"),
+                )
+                + 1
+            )
+
+
+# def rhs(graph: nx.DiGraph, vertex, d: List):
+#     # todo добавить параметр start_vertex в метод update
+#     if vertex == 0:
+#         return 0
+#     dists = [d[v] for v in graph.predecessors(vertex)]
+#     if not dists:
+#         return float('inf')
+#     else:
+#         return min(dists) + 1
+#
+#
+# def dynamic_dijkstra(graph: nx.DiGraph, q: List, d: List):
+#     while q:
+#         _, u = heapq.heappop(q)
+#         if rhs(graph, u, d) < d[u]:
+#             d[u] = rhs(graph, u, d)
+#         elif d[u] < rhs(graph, u, d):
+#             d[u] = float('inf')
+#
+#
+# def process_subtree(graph: nx.DiGraph, root, d: List, spt: nx.DiGraph):
+#     s = set()
+#     cur = set()
+#     if rhs(graph, root, d) != d[root]:
+#         cur.add(root)
+#     while cur:
+#         new_cur = []
+#         for u in cur:
+#             if rhs(graph, u, d) < d[u]:
+#                 d[u] = rhs(graph, u, d)
+#                 s.add(u)
+#             elif d[u] < rhs(graph, u, d):
+#                 d[u] = float('inf')
+#                 s.add(u)
+#             if u not in s:
+#                 continue
+#             for v in graph.successors(u):
+#                 if v not in s and v not in cur:
+#                     new_cur.append(v)
+#         cur = new_cur
+#
+#     for v in s.union({root}):
+#         if d[v] == float('inf'):
+#             spt.remove_node(v)
+#         else:
+#             dists = [(d[u], u) for u in graph.predecessors(v)]
+#             if not dists:
+#                 raise Exception('у вершины с d != +inf обязан быть предок')
+#             _, u = min(dists)
+#             spt.add_edge(u, v)
+#
+#
+# def update3(updated_edge, updated_graph: nx.DiGraph, spt: nx.DiGraph, d: List):
+#     # в graph уже должны быть удалены/добавлены ребра, это уже пост обработка
+#     (u, v) = updated_edge
+#     if (u, v) in spt.edges:
+#         if d[u] + 1 == d[v]:
+#             d[v] = float('inf')
+#             spt.remove_edge(u, v)
+#         process_subtree(updated_graph, v, d, spt)
+#     else:
+#         # 1: удаляем ребро не из spt, тогда просто pass
+#         # 2: добавляем новое ребро, тогда нужна проверка
+#         if (u, v) in updated_graph.edges:
+#             if d[u] + 1 < d[v]:
+#                 process_subtree(updated_graph, v, d, spt)
--- tests/task_9/test_dijkstra.py
+++ tests/task_9/test_dijkstra.py
@@ -1,32 +1,109 @@
+import random
+
 import networkx as nx
 from pygraphblas import Matrix
-from project.task_9_draft import dijkstra
+from project.task_9_draft import dijkstra, DynamicSSSP
 from project.sssp import bellman_ford
 
 
 def test_dijkstra():
-    DG = nx.DiGraph()
-    DG.add_edge(0, 1)
-    DG.add_edge(1, 3)
-    DG.add_edge(3, 1)
-    DG.add_edge(2, 0)
-    actual = dijkstra(DG, 0)
+    # ручной тест
+    g = nx.DiGraph()
+    g.add_edge(0, 1)
+    g.add_edge(1, 3)
+    g.add_edge(3, 1)
+    g.add_edge(2, 0)
+    actual = dijkstra(g, 0)
     assert actual == [0, 1, float("inf"), 2]
 
-    actual = dijkstra(DG, 1)
+    actual = dijkstra(g, 1)
     assert actual == [float("inf"), 0, float("inf"), 1]
 
-    actual = dijkstra(DG, 2)
+    actual = dijkstra(g, 2)
     assert actual == [1, 2, 0, 3]
 
-    actual = dijkstra(DG, 3)
+    actual = dijkstra(g, 3)
     assert actual == [float("inf"), 1, float("inf"), 0]
 
     # тест с использованием ранее реализованного алгоритма bellman_ford
-    for i in range(1, 150):
-        g = nx.DiGraph(nx.generators.atlas.graph_atlas(i))
+    for i in range(1, 500):
+        g = nx.to_directed(nx.generators.atlas.graph_atlas(i))
         adj_matrix = Matrix.from_scipy_sparse(nx.adjacency_matrix(g))
         for s in g.nodes:
             actual = dijkstra(g, s)
             expected = bellman_ford(adj_matrix, s)
             assert actual == expected
+
+
+def test_dynamic():
+    for i in range(1, 500):
+        modifiable_graph: nx.DiGraph = nx.DiGraph(nx.generators.atlas.graph_atlas(i))
+
+        edges = list(modifiable_graph.edges)
+        del_edges_num = len(edges) // 2
+        add_edges_num = len(edges) - del_edges_num
+
+        # select edges that will be added to graph during test and fix their future insertion order
+        add_edges = []
+        for _ in range(add_edges_num):
+            i = random.randint(0, len(edges) - 1)
+            u, v = edges[i]
+            add_edges.append(edges[i])
+            modifiable_graph.remove_edge(u, v)
+            del edges[i]
+
+        # select edges that will be deleted from graph during test and fix their future deletion order
+        del_edges = list(modifiable_graph.edges)
+        # initialize distances for dynamic algorithm
+        dynamic_sssp = DynamicSSSP(modifiable_graph, 0)
+
+        while add_edges or del_edges:
+            # add or delete edges in arbitrary order
+            if add_edges and random.random() < 0.5:
+                u, v = add_edges.pop()
+                dynamic_sssp.add_edge(u, v)
+            elif del_edges:
+                u, v = del_edges.pop()
+                dynamic_sssp.remove_edge(u, v)
+            else:
+                u, v = add_edges.pop()
+                dynamic_sssp.add_edge(u, v)
+
+            dynamic_sssp.update()
+            expected = dijkstra(modifiable_graph, 0)
+
+            assert dynamic_sssp.d == expected
+
+
+def test_dynamic_decremental():
+    for i in range(1, 500):
+        modifiable_graph: nx.DiGraph = nx.DiGraph(nx.generators.atlas.graph_atlas(i))
+        dynamic_sssp = DynamicSSSP(modifiable_graph, 0)
+        edges = list(modifiable_graph.edges)
+        while edges:
+            i = random.randint(0, len(edges) - 1)
+            u, v = edges[i]
+            del edges[i]
+
+            dynamic_sssp.add_edge(u, v)
+            dynamic_sssp.update()
+            expected = dijkstra(modifiable_graph, 0)
+
+            assert dynamic_sssp.d == expected
+
+
+def test_dynamic_incremental():
+    for i in range(1, 500):
+        g1: nx.DiGraph = nx.DiGraph(nx.generators.atlas.graph_atlas(i))
+
+        modifiable_graph = nx.DiGraph()
+        modifiable_graph.add_nodes_from(g1.nodes)
+
+        dynamic_sssp = DynamicSSSP(modifiable_graph, 0)
+        for u, v in g1.edges:
+            dynamic_sssp.add_edge(u, v)
+
+            dynamic_sssp.update()
+            expected = dijkstra(modifiable_graph, 0)
+
+            assert dynamic_sssp.d == expected
